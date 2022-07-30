090c3c3bfee1f5f8e1368d04eebbaf10686d6dad
squad212 <kcsaroj62@gmail.com>
2022-07-30 15:07:20 -0500

    utilized the local variable

--- src/main/java/com/humana/dhp/dhppocs/BasicJava/com/oops/Encapsulation/Pharmacy.java
+++ src/main/java/com/humana/dhp/dhppocs/BasicJava/com/oops/Encapsulation/Pharmacy.java
@@ -1,15 +1,14 @@
 package com.humana.dhp.dhppocs.BasicJava.com.oops.Encapsulation;
 
 public class Pharmacy {
-    /*
-    pharmacy class has medicine and gives the total of two medicine
-     */
+  private String medicine1 = "Ibrubrofen";
+    private String medicine2 = "Advil";
 
 
     public  int priceBasedMedicine(String nameOfMedicine) {
-        if (nameOfMedicine.equalsIgnoreCase("Ibrubrofen")) {
+        if (nameOfMedicine.equalsIgnoreCase(medicine1)) {
             return 20;
-        } else if (nameOfMedicine.equalsIgnoreCase("Advil")) {
+        } else if (nameOfMedicine.equalsIgnoreCase(medicine2)) {
             return 42;
         } else {
             return 0;
